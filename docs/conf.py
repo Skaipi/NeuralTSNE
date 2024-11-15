6ceced45b08a693fd7eae5890dad83362c35d229
Minosyx <minosyx@gmail.com>
2024-11-15 23:41:58 +0100

    Enhance Sphinx documentation configuration to support dynamic GitHub link resolution and versioning

--- docs/conf.py
+++ docs/conf.py
@@ -8,6 +8,11 @@
 
 import os
 import sys
+import sphinx
+import sphinx.application
+import sphinx.config
+import sphinx.environment
+import sphinx.util
 
 sys.path.insert(0, os.path.abspath("../NeuralTSNE"))
 
@@ -16,7 +21,9 @@ project = "NeuralTSNE"
 copyright = "2024, Patryk Tajs"
 author = "Patryk Tajs"
 release = "1.0.0"
-current_version = "1.0.0"
+current_version = "v1.0.0"
+smv_current_version = "v1.0.0"
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -34,10 +41,6 @@ extensions = [
 ]
 
 
-def setup(app):
-    current_version = app.config.smv_current_version
-
-
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
@@ -50,7 +53,6 @@ smv_prefer_remote_refs = True
 add_module_names = False
 autodoc_typehints = "signature"
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
@@ -58,13 +60,26 @@ default_dark_mode = True
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
+linkcode_url = "https://github.com/placeholder/NeuralTSNE/"
 
-github_user = os.getenv("GITHUB_USER", "")
-github_repo = os.getenv("GITHUB_REPO", "")
 
-linkcode_url = (
-    f"https://github.com/{github_user}/{github_repo}/blob/{current_version}/NeuralTSNE/"
-)
+def on_config_inited(app: sphinx.application.Sphinx, config) -> None:
+    global linkcode_url
+    if hasattr(config, "smv_current_version"):
+        smv_current_version = config.smv_current_version
+        github_user = os.getenv("GITHUB_USER", "")
+        github_repo = os.getenv("GITHUB_REPO", "")
+        linkcode_url = f"https://github.com/{github_user}/{github_repo}/blob/{smv_current_version}/NeuralTSNE/"
+        config.linkcode_url = linkcode_url
+
+
+def setup(app):
+    app.connect("config-inited", on_config_inited)
+    return {
+        "version": "0.1",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
 
 
 def linkcode_resolve(domain, info):
